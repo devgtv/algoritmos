3e34e5be461971c2eabd994dc6f09f095911e432
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-03-06 18:29:17 -0300

    ex1212

---
+++ ex1177.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int t;
+	cin >> t;
+	for (int i = 0; i < 1000; i++) {
+		cout << "N[" << i << "] = " << i % t << endl;
+
+
+
+	}
+
+
+	return 0;
+}
