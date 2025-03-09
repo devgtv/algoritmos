ff55516f5900a97e86c4772f0d7794c551fe7446
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-03-09 14:32:56 -0300

    3 exercicios saindo do forno hehe

--- ex1129.cpp
+++ ex1129.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
-
 int main() {
-	string alfabeto;
-	int n;
 
-	while (cin >> alfabeto) {
 
-		cin >> n;
-		vector<int> indeces(n);
-		for (int i = 0; i < n; i++) {
-			cin >> indeces[i];
-		}
-		for (int i = 0; i < n; i++) {
-			cout << alfabeto[indeces[i] - 1];
-		}
-		cout << endl;
-	}
 
 	return 0;
 }
