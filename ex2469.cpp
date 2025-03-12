712430ba6370b3062b78dd0b3f04ea24fd7ffb56
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-03-12 18:47:46 -0300

    nOtas

--- ex2469.cpp
+++ ex2469.cpp
@@ -1,16 +1,42 @@
-#include <iostream> 
+#include <iostream>
 #include <vector>
+
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
+    int N;
+    cin >> N;
+    vector<int> notas(N);
 
-	vector<int> notas[n];
+    for (int i = 0; i < N; i++) {
+        cin >> notas[i];
+    }
 
 
+    int nota_maxima = 100;
 
 
+    vector<int> frequencia(nota_maxima + 1, 0);
 
-	return 0;
+
+    for (int i = 0; i < N; i++) {
+        frequencia[notas[i]]++;
+    }
+
+
+    int nota_mais_frequente = -1;
+    int max_frequencia = 0;
+
+
+    for (int i = 0; i <= nota_maxima; i++) {
+        if (frequencia[i] > max_frequencia || (frequencia[i] == max_frequencia && i > nota_mais_frequente)) {
+            max_frequencia = frequencia[i];
+            nota_mais_frequente = i;
+        }
+    }
+
+
+    cout << nota_mais_frequente << endl;
+
+    return 0;
 }
