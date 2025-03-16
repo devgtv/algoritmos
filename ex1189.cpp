d2566115f5275b0f6c100e98a6c92173bbea33f4
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-03-16 09:33:56 -0300

    codigos matinais hehe

---
+++ ex1189.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	char soun;
+	double matriz[12][12];
+	double soma = 0.0;
+	int contador = 0;
+
+	cin >> soun;
+	for (int i = 0; i < 12; i++) {
+		for (int j = 0; j < 12; j++) {
+			cin >> matriz[i][j];
+		}
+	}
+
+	for (int i = 0; i < 12; ++i) {
+		for (int j = 0; j < i; ++j) {
+			if (i > j && i < 12 - j - 1)
+			{
+
+				soma += matriz[i][j];
+				++contador;
+			}
+		}
+	}
+	if (soun == 'S') {
+
+		cout << fixed << setprecision(1) << soma << endl;
+	}
+	else if (soun == 'M') {
+
+		cout << fixed << setprecision(1) << soma / contador << endl;
+	}
+
+
+
+	return 0;
+}
--- ex1190.cpp
+++ ex1190.cpp
@@ -1,20 +1,36 @@
-// ex1190.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    double m[12][12], soma = 0, media = 0;
+    char soun;
+    int contador = 0;
+
+    cin >> soun;
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            cin >> m[i][j];
+        }
+    }
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            if (i < j && i > 12 - j - 1) {  
+                soma += m[i][j];
+                contador++;
+            }
+        }
+    }
+
+    if (soun == 'S') {
+        cout << fixed << setprecision(1) << soma << endl;
+    }
+    else if (soun == 'M') {
+        
+        cout << fixed << setprecision(1) <<  soma / contador << endl;
+    }
+
+    return 0;
 }
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
