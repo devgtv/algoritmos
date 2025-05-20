04712e68d216f59b6c6e99345f01c10b1c6e051e
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-05-19 21:53:09 -0300

    dfg

---
+++ ex1155.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std; 
+
+int main() {
+
+	double s = 0;
+	
+	for (int i = 1; i <= 100; i++) {
+		s =  (1.0/i) + s ;
+
+		
+	}
+
+	cout << fixed << setprecision(2) <<  s  << endl;
+
+
+
+
+	return 0;
+}
