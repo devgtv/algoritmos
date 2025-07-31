47a7ff87b08f0293f89923c32249fab48850ba99
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-07-31 13:07:19 -0300

    bit++

---
+++ Bit++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    
+    int x = 0;
+    for(int i = 0 ; i < n;i++) {
+        string statements;
+        cin >> statements;
+
+        if (statements == "++X" || statements == "X++") {
+            x++;
+        } else if (statements == "--X" || statements == "X--") {
+            x--;
+        }
+    }
+    cout << x << endl;
+    return 0;
+}
