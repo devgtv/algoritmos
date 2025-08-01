8eb7f84dcf06ad52f7d7f33f553e08c6f89e1622
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-08-01 11:55:34 -0300

    DOmino pilling

---
+++ Domino piling.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int m,n;
+    // (m * n) / 2.
+    cin >> m >> n;
+
+
+    int maximo = (m * n) / 2;
+    cout << maximo << endl;
+
+    return 0;
+}
