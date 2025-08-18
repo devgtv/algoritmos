c6c3d128df9da20a1fd6d05686bf06850415e26b
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-08-18 18:02:27 -0300

    Elephant

---
+++ Elephant.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x;
+    cin >> x;
+
+    int steps = x / 5;
+    if (x % 5 != 0) {
+        steps += 1;
+    }
+
+    cout << steps << '\n';
+
+    return 0;
+}
