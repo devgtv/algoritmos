9d304bb6a76c7aa603612518dd181cc2592c87f6
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-08-18 17:54:52 -0300

    Bear and Big Brother

---
+++ Bear and Big Brother.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main () {
+    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a,b;
+    cin >> a >> b;
+    int years = 0;
+
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        years++;
+    }
+    cout << years << endl;
+    return 0; 
+
+}
