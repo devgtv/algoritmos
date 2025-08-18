f85c0b7326ea48f8deebcc4324c1d5eb40916cca
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-08-18 16:30:04 -0300

    linda matriz

---
+++ Beautiful Matrix.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int abs(int x) {
+    return (x < 0) ? -x : x;
+}
+
+int main() {
+    int matriz[5][5];
+    int linha = 0, coluna = 0;
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            cin >> matriz[i][j];
+            if (matriz[i][j] == 1) {
+                linha = i;
+                coluna = j;
+            }
+        }
+    }
+    int movimentos = abs(linha - 2) + abs(coluna - 2);
+    cout << movimentos << endl;
+
+    return 0;
+}
