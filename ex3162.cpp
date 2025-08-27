f2ab993233a2c163f1817df4800d46aa464c9e7c
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-08-27 16:22:33 -0300

    ga

---
+++ ex3162.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+double CalcularDistancia(const vector<int>& a, const vector<int>& b) {
+    int dx = a[0] - b[0];
+    int dy = a[1] - b[1];
+    int dz = a[2] - b[2];
+
+    return sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> naves(n, vector<int>(3));
+
+    for (int i = 0; i < n; ++i) {
+        cin >> naves[i][0] >> naves[i][1] >> naves[i][2];
+    }
+
+    for (int i = 0; i < n; ++i) {
+        double MenorDistancia = 1e9; 
+
+        for (int j = 0; j < n; ++j) {
+            
+
+            double dist = CalcularDistancia(naves[i], naves[j]);
+            if (dist < MenorDistancia) {
+                MenorDistancia = dist;
+            }
+        }
+
+        if (MenorDistancia <= 20.0) {
+            cout << "A" << endl;
+        } else if (MenorDistancia <= 50.0) {
+            cout << "M" << endl;
+        } else {
+            cout << "B" << endl;
+        }
+    }
+
+    return 0;
+}
