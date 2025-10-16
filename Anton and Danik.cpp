02ce2dea7c6a0bbb9c7fd34caaadfc8410d3832f
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-10-16 14:42:07 -0300

    anton and danik

---
+++ Anton and Danik.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int aw = 0;
+    int dw = 0;
+    for(char c : s) {
+        if (c == 'A') aw++;
+        else if (c == 'D')dw++;
+    }
+
+    if(aw > dw) {
+        cout << "Anton\n";
+    } else if (dw > aw) {
+        cout << "Danik\n";
+    } else {
+        cout << "Friendship\n";
+    }
+    return 0;
+}
