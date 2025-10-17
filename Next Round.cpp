04e844d584cf29b35d300cce6fc0a1174570973c
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-10-17 08:31:34 -0300

    klahsd

--- Next Round.cpp
+++ Next Round.cpp
@@ -1,12 +1,25 @@
-#include <iostream>
-using namespace std;
-int main () {
-
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-
-
-    
-    return 0;
-}
+    #include <iostream>
+    using namespace std;
+    int main () {
+     
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int n,k;
+        cin >> n >> k;
+        int pontuacao [50];
+        for(int i = 0 ; i < n;i++) {
+            cin >> pontuacao[i];
+        }
+     
+        int threshold = pontuacao[k-1];
+        int contador = 0;
+     
+        for (int i = 0 ; i < n;i++) {
+            if(pontuacao[i] >= threshold && pontuacao[i] > 0) {
+                contador++;
+            }
+        }
+        cout << contador << endl;
+     
+        return 0;
+    }
