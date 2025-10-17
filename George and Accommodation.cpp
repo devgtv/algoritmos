5aab07ddc9c862e64f7df97bf63425bb7cd41975
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-10-17 20:13:41 -0300

    knsadfd

---
+++ George and Accommodation.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+signed main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int contador = 0;
+    int n ;
+    cin >> n;
+    for(int i = 0 ; i < n;i++) {
+        int x,y;
+        cin >> x >> y;
+        if(y - x >= 2) {
+            contador++;
+        }
+    }
+    cout << contador << endl;
+    
+
+    return 0;
+}
