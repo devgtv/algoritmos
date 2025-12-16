4c3f8105de623e0a2da1eb436496802fb56990a5
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-12-16 16:06:03 -0300

    ajuste

--- beecrowd/ex3104.cpp
+++ beecrowd/ex3104.cpp
@@ -1,24 +1,19 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false); 
-    cin.tie(nullptr); 
+signed main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     string a;
-    long long b;
-
+    int b;
     cin >> a >> b;
-
-    long long remainder = 0;
-
-    for (char digit : a) {
-        
+    int remainder = 0;
+    for(char digit : a) {
         remainder = (remainder * 10 + (digit - '0')) % b;
     }
-
     cout << remainder << endl;
-
     return 0;
 }
