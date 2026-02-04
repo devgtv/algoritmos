4907264da6ced23ab1becf041a92110bd48fb88b
devgtv <gustavobarrosfialho1402gb@gmail.com>
2026-02-03 21:33:09 -0300

    gamewithintegfers

---
+++ codeforces/gamewithintegers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+
+signed main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        if(n%3 ==0) {
+            cout << "Second" << endl;
+        } else {
+            cout << "First" << endl;
+        }
+    }
+    
+    return 0;
+}
