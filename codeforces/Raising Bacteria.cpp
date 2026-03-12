6a546e5920aa59c575e8abb3d2ca7190c30e4e1e
devgtv <gustavobarrosfialho1402gb@gmail.com>
2026-03-12 11:32:17 -0300

    raising bacteria

---
+++ codeforces/Raising Bacteria.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+ 
+signed main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x;
+    cin >> x;
+    cout << __builtin_popcount(x) << endl;
+
+    
+    return 0;
+}
