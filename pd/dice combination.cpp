d121568a05d34a7c62406e249332d694d6222ee4
devgtv <gustavobarrosfialho1402gb@gmail.com>
2026-04-05 17:18:21 -0300

    dice combinations

---
+++ pd/dice combination.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+
+    int mod = 1e9 + 7;
+    vector<int> dp(n+ 1,0);
+
+    dp[0] = 1;
+
+    for(int i = 1; i <= n;i++) {
+        for(int j = 1; j <=6;j++) {
+            if(i - j >= 0) {
+                dp[i] = (dp[i] + dp[i - j]) % mod;
+            }
+        }
+    }
+    
+    cout << dp[n] << endl;
+    return 0;
+}
